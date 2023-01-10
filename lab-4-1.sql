f15eed16b44582a0317106dc3c0ab56a26744e6f
Hopcus <alyssa.hopcus@kellogg.northwestern.edu>
2023-01-10 22:23:21 +0000

    labs 1 through 4.2 in class

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,27 @@
 -- How many lifetime hits does Barry Bonds have?
 
+-- First I did it as two steps, running the following:
+-- SELECT id, first_name, last_name
+-- FROM players
+-- WHERE last_name = "Bonds";
+-- this returned:
+
+-- id       first_name  last_name 
+----------  ----------  ----------
+-- 1678        Barry       Bonds     
+-- 1679        Bobby       Bonds  
+
+-- THEN
+-- SELECT SUM(hits)
+-- FROM stats
+-- WHERE player_id = 1678;
+-- what i needed to do was USE A JOIN to pull from both tables at once
+
+SELECT SUM(stats.hits)
+FROM stats INNER JOIN players ON players.id = stats.player_id
+WHERE players.first_name = "Barry"
+AND players.last_name = "Bonds";
+
 -- Expected result:
 -- 2935
 
